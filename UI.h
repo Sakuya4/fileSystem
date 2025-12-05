f0a40c39a0ea7fff2b8db024bb4464b307ad8ce8
sakuya4 <janalexei88@gmail.com>
2025-12-05 23:03:29 +0800

    define function

---
+++ UI.h
@@ -0,0 +1,33 @@
+#ifndef UI_H
+#define UI_H
+
+
+int options();
+int getCommand();
+
+/* base command */
+int lsCommand(); // list
+int cdCommand(); // change directory
+int rmCommand(); // remove file 
+int mkdirCommand(); // make directory
+int rmdirCommand(); // remove directory
+int putsCommand(); // upload file
+int getsCommand(); // download file
+int catCommand(); // display file content
+int statusCommand(); // display file status
+int helpCommand(); // display help information
+int exitCommand(); // exit program
+/* base command done */
+
+/* adv command */
+int vimCommand();// VIM editor command
+/* adv command done */
+
+/* cybersecurity command */
+int hashCommand(); // file hash command
+/* cybersecurity command done */
+
+#endif
+
+
+
